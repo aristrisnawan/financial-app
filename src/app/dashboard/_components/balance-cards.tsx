ebe169573fd9c9e91ecf098c0459a1dafff98055
aristrisnawanat <aristrisnawanat@telkomuniversity.ac.id>
2026-08-03 07:14:53 +0700

    Test connection supabase

---
+++ src/app/dashboard/_components/balance-cards.tsx
@@ -0,0 +1,68 @@
+"use client";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getBalanceSummary } from "@/features/action";
+import { convertToIDR } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { TrendingDownIcon, TrendingUpDownIcon, WalletIcon } from "lucide-react";
+
+export function BalanceCard() {
+  const { data, error } = useQuery({
+    queryKey: ["balance"],
+    queryFn: () => getBalanceSummary(),
+  });
+
+  if (error) {
+    return (
+      <div className="w-full p-4 text-sm border rounded-lg border-destructive/50 text-destructive bg-destructive/10">
+        Failed to get balance
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-4 mb-8 md:grid-cols-3">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2 text-primary">
+            <WalletIcon className="size-4" />
+            Saving
+          </CardTitle>
+          <CardDescription className="text-2xl font-semibold text-secondary-foreground">
+            {convertToIDR(Number(data?.savings || 0))}
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="text-sm">Savings for all time</CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2 text-primary">
+            <TrendingUpDownIcon className="size-4" />
+            Incomes
+          </CardTitle>
+          <CardDescription className="text-2xl font-semibold text-secondary-foreground">
+            {convertToIDR(Number(data?.totalIncome || 0))}
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="text-sm">Total incomes for all time</CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2 text-primary">
+            <TrendingDownIcon className="size-4" />
+            Expenses
+          </CardTitle>
+          <CardDescription className="text-2xl font-semibold text-secondary-foreground">
+            {convertToIDR(Number(data?.totalExpense || 0))}
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="text-sm">Total expenses for all time</CardFooter>
+      </Card>
+    </div>
+  );
+}
