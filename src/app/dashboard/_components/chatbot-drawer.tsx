c7c1dfde7c943925362b3de1d0ac95a7ddb4988b
aristrisnawanat <aristrisnawanat@telkomuniversity.ac.id>
2026-08-13 21:06:28 +0700

    Create UI AI Chatbot

--- src/app/dashboard/_components/chatbot-drawer.tsx
+++ src/app/dashboard/_components/chatbot-drawer.tsx
@@ -1,22 +1,117 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { handleChat } from "@/features/ai/chat";
-import { BotIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { BotIcon, XIcon } from "lucide-react";
+import { useState } from "react";
+import ChatbotTextarea from "./chatbot-textarea";
 
 export default function ChatbotDrawer() {
+  const [conversation, setConversation] = useState<
+    {
+      role: string;
+      parts: {
+        text: string;
+      }[];
+    }[]
+  >([
+    {
+      role: "user",
+      parts: [
+        {
+          text: "Hello",
+        },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
+        {
+          text: "Hello, how can i help you?",
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="fixed bottom-4 right-4">
-      <Button
-        className="size-14 rounded-full bg-primary text-white flex items-center justify-center"
-        size="icon-lg"
-        variant="outline"
-        onClick={async () => {
-          const result = await handleChat("halo");
-          console.log(result);
-        }}
-      >
-        <BotIcon className="size-6" />
-      </Button>
-    </div>
+    <Drawer direction="right" modal={false}>
+      <DrawerTrigger className="fixed bottom-4 right-4" asChild>
+        <Button
+          className="rounded-full size-14"
+          size="icon-lg"
+          variant="outline"
+          //   onClick={async () => {
+          //     const result = await handleChat();
+          //     console.log(result);
+          //   }}
+        >
+          <BotIcon className="size-6" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className=" w-screen! md:w-110!">
+        <DrawerHeader className=" flex flex-row justify-between">
+          <div>
+            <DrawerTitle className=" text-primary font-bold">
+              AI Financial Advisor
+            </DrawerTitle>
+            <DrawerDescription>
+              Get personelize financial advice.
+            </DrawerDescription>
+          </div>
+          <DrawerClose asChild>
+            <Button variant="outline" size="icon">
+              <XIcon />
+            </Button>
+          </DrawerClose>
+        </DrawerHeader>
+        <div className="no-scrollbar overflow-y-auto px-4 h-full">
+          {conversation.length > 0 ? (
+            <div className="flex flex-col h-full overflow-x-hidden no-scrollbar overflow-y-auto gap-8">
+              {conversation.map((message, index) => (
+                <div
+                  key={`conversation-${index}`}
+                  className={cn(
+                    "flex flex-col gap-2",
+                    message.role === "model" ? "items-start" : "items-end",
+                  )}
+                >
+                  <div
+                    className={cn("flex flex-col w-full", {
+                      "bg-primary/20 px-5 py-2 rounded-3xl rounded-br-md w-fit max-w-3/4 text-primary":
+                        message.role === "user",
+                    })}
+                  >
+                    {message.role === "model" && (
+                      <div className="flex items-center text-xs gap-1 text-primary font-semibold">
+                        <BotIcon />
+                        AI Advisor
+                      </div>
+                    )}
+                    {message.parts[0].text}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full">
+              <h2 className=" text-3xl font-bold text-primary">Hello There</h2>
+              <h4 className="text-xl">What can I help you?</h4>
+            </div>
+          )}
+        </div>
+        <DrawerFooter>
+          <ChatbotTextarea />
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
---
+++ src/app/dashboard/_components/chatbot-textarea.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SendIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+  message: z.string().min(1, "Message is required"),
+});
+export default function ChatbotTextarea() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    form.reset();
+  }
+  return (
+    <form className="flex flex-col bg-secondary rounded-2xl p-2">
+      <Controller
+        control={form.control}
+        name="message"
+        render={({ field, fieldState }) => (
+          <Field>
+            <textarea
+              {...field}
+              id="form-message"
+              placeholder="Ask AI Advisor here"
+              autoComplete="off"
+              className="h-16 resize-none rounded-md px-3 py-2 focus:outline-none"
+            />
+          </Field>
+        )}
+      />
+      <div className="flex justify-between">
+        <div></div>
+        <div>
+          <Button
+            type="submit"
+            size="icon"
+            variant="ghost"
+            className="text-primary hover:bg-primary/10 disabled:bg-transparent hover:text-primary cursor-pointer"
+          >
+            <SendIcon className="size-5" />
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+}
